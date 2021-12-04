36adf1de5df92c2aa30556f687c5aa42bac66ff2
Godlux <mike-98@yandex.ru>
2021-12-04 18:35:21 +0300

    Uncomment some lines

--- knaps_helper.py
+++ knaps_helper.py
@@ -66,49 +66,49 @@ if __name__ == "__main__":
     m = 128
     result_folder = "./temp_result_data"
 
-    # # stage1
-    # stage = 1
-    # for processorCount in [1, 2, 4, 6, 8, 10, 12, 14, 16]:
-    #     t0 = time.time()
-    #     command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m}"
-    #     print(f"Executing: {command}")
-    #     data = run_command(command)
-    #     print(data)
-    #     t1 = time.time()
-    #     # pasre program output
-    #     parsed_data = parse_data(data)
-    #     print(parsed_data)
-    #     filename = f"result-{stage}-{processorCount}"
-    #     # saving data to json format
-    #     save_json(result_folder, parsed_data, filename)
-    #     # saving data to excel format
-    #     save_xlsx(result_folder, parsed_data, filename)
-    #     # print elspsed time
-    #     elapsedTime = t1 - t0
-    #     print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
-    #     log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
+    # stage1
+    stage = 1
+    for processorCount in [1, 2, 4, 6, 8, 10, 12, 14, 16]:
+        t0 = time.time()
+        command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m}"
+        print(f"Executing: {command}")
+        data = run_command(command)
+        print(data)
+        t1 = time.time()
+        # pasre program output
+        parsed_data = parse_data(data)
+        print(parsed_data)
+        filename = f"result-{stage}-{processorCount}"
+        # saving data to json format
+        save_json(result_folder, parsed_data, filename)
+        # saving data to excel format
+        save_xlsx(result_folder, parsed_data, filename)
+        # print elspsed time
+        elapsedTime = t1 - t0
+        print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
+        log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
 
-    # # stage2
-    # stage = 2
-    # for processorCount in [1, 2, 4, 6, 8, 10, 12, 14, 16]:
-    #     t0 = time.time()
-    #     command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m} -o"
-    #     print(f"Executing: {command}")
-    #     data = run_command(command)
-    #     print(data)
-    #     t1 = time.time()
-    #     # pasre program output
-    #     parsed_data = parse_data(data)
-    #     print(parsed_data)
-    #     filename = f"result-{stage}-{processorCount}"
-    #     # saving data to json format
-    #     save_json(result_folder, parsed_data, filename)
-    #     # saving data to excel format
-    #     save_xlsx(result_folder, parsed_data, filename)
-    #     # print elspsed time
-    #     elapsedTime = t1 - t0
-    #     print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
-    #     log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
+    # stage2
+    stage = 2
+    for processorCount in [1, 2, 4, 6, 8, 10, 12, 14, 16]:
+        t0 = time.time()
+        command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m} -o"
+        print(f"Executing: {command}")
+        data = run_command(command)
+        print(data)
+        t1 = time.time()
+        # pasre program output
+        parsed_data = parse_data(data)
+        print(parsed_data)
+        filename = f"result-{stage}-{processorCount}"
+        # saving data to json format
+        save_json(result_folder, parsed_data, filename)
+        # saving data to excel format
+        save_xlsx(result_folder, parsed_data, filename)
+        # print elspsed time
+        elapsedTime = t1 - t0
+        print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
+        log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
 
     # # stage3.1
     stage = 3
@@ -133,27 +133,27 @@ if __name__ == "__main__":
         print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
         log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
 
-    # # stage3.2
-    # stage = 3
-    # for relWeight in range(10, 110, 10):
-    #     processorCount = 16
-    #     t0 = time.time()
-    #     command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m} -r {relWeight} -o"
-    #     print(f"Executing: {command}")
-    #     data = run_command(command)
-    #     print(data)
-    #     t1 = time.time()
-    #     # pasre program output
-    #     parsed_data = parse_data(data)
-    #     ##print(parsed_data)
-    #     filename = f"result-{stage}-{relWeight}"
-    #     # saving data to json format
-    #     save_json(result_folder, parsed_data, filename)
-    #     # saving data to excel format
-    #     save_xlsx(result_folder, parsed_data, filename)
-    #     # print elspsed time
-    #     elapsedTime = t1 - t0
-    #     print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
-    #     log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
+    # stage3.2
+    stage = 3
+    for relWeight in range(10, 110, 10):
+        processorCount = 16
+        t0 = time.time()
+        command = f"./KnapsackTree -n {n} -i {iterCount} -p {processorCount} -m {m} -r {relWeight} -o"
+        print(f"Executing: {command}")
+        data = run_command(command)
+        print(data)
+        t1 = time.time()
+        # pasre program output
+        parsed_data = parse_data(data)
+        ##print(parsed_data)
+        filename = f"result-{stage}-{relWeight}"
+        # saving data to json format
+        save_json(result_folder, parsed_data, filename)
+        # saving data to excel format
+        save_xlsx(result_folder, parsed_data, filename)
+        # print elspsed time
+        elapsedTime = t1 - t0
+        print(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!")
+        log_file.write(f"Stage {stage} Finished! Elapsed: {elapsedTime} s!\n")
 
 log_file.close()
